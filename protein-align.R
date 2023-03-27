c9cd863195f665316a77a9096d4e02d8b1176918
Luciano Kalabric <luciano.kalabric@fiocruz.br>
2023-03-27 08:06:26 -0300

    Update protein-align.R

--- protein-align.R
+++ protein-align.R
@@ -8,13 +8,28 @@ if (!require("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
 
 BiocManager::install("ensembldb")
-
-if (!require("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
-
 BiocManager::install("EnsDb.Hsapiens.v86")
+BiocManager::install("Gviz")
 
 library(ensembldb)
 library(EnsDb.Hsapiens.v86)
 
 edbx <- filter(EnsDb.Hsapiens.v86, filter = ~ seq_name == "X")
+gnm <- GRanges("X:107716399-107716401")
+
+library("Gviz")
+## Since we're using Ensembl chromosome names we have to set:
+options(ucscChromosomeNames = FALSE)
+
+## Define a genome axis track
+gat <- GenomeAxisTrack(range = gnm)
+
+## Get all genes in that region
+gnm_gns <- getGeneRegionTrackForGviz(edbx, filter = GRangesFilter(gnm))
+gtx <- GeneRegionTrack(gnm_gns, name = "tx", geneSymbol = TRUE,
+                       showId = TRUE)
+
+## Generate a higlight track
+ht <- HighlightTrack(trackList = list(gat, gtx), range = gnm)
+## plot the region
+plotTracks(list(ht))
