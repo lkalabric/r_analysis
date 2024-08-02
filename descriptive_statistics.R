84f6e5e615289ddb6a95106ca2a10c3f43b32272
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2024-08-02 19:04:19 -0300

    Update descriptive_statistics.R

--- descriptive_statistics.R
+++ descriptive_statistics.R
@@ -12,55 +12,73 @@ library(dplyr)
 library(readr)
 library(ggpubr)
 
+###
+### Análise Urban Schisto - Tabela 1
+###
+
 # Carregando os dados de um arquivo .csv
-#Tabela_1 <- read_csv("C:/Users/luciano.kalabric/OneDrive - FIOCRUZ/Projetos/2014- Mitermayer Esquistossomose urbana/Produção/Publicações/2023 Artigo Urban Schisto/Tabela 1 - SA2018.csv")
-Tabela_1 <- read.csv("C:/Users/luciano.kalabric/OneDrive - FIOCRUZ/Projetos/2014- Mitermayer Esquistossomose urbana/Produção/Publicações/2023 Artigo Urban Schisto/Tabela 1 - All.csv")
+#data <- read.csv("C:/Users/luciano.kalabric/OneDrive - FIOCRUZ/Projetos/2014- Mitermayer Esquistossomose urbana/Produção/Publicações/2023 Artigo Urban Schisto/Tabela 1 - SB2011.csv")
+#data <- read.csv("C:/Users/luciano.kalabric/OneDrive - FIOCRUZ/Projetos/2014- Mitermayer Esquistossomose urbana/Produção/Publicações/2023 Artigo Urban Schisto/Tabela 1 - DC2017.csv")
+data <- read.csv("C:/Users/luciano.kalabric/OneDrive - FIOCRUZ/Projetos/2014- Mitermayer Esquistossomose urbana/Produção/Publicações/2023 Artigo Urban Schisto/Tabela 1 - SA2018.csv")
+#data <- read.csv("C:/Users/luciano.kalabric/OneDrive - FIOCRUZ/Projetos/2014- Mitermayer Esquistossomose urbana/Produção/Publicações/2023 Artigo Urban Schisto/Tabela 1 - PI2019.csv")
+#data <- read.csv("C:/Users/luciano.kalabric/OneDrive - FIOCRUZ/Projetos/2014- Mitermayer Esquistossomose urbana/Produção/Publicações/2023 Artigo Urban Schisto/Tabela 1 - LP2022.csv")
+#data <- read.csv("C:/Users/luciano.kalabric/OneDrive - FIOCRUZ/Projetos/2014- Mitermayer Esquistossomose urbana/Produção/Publicações/2023 Artigo Urban Schisto/Tabela 1 - BF2023.csv")
 
-# Para confirmar a classe de uam variável
-class(Tabela_1)
+# Para confirmar a classe de uma variável
+class(data)
 
-### Exemplo de comandos básicos
-# Frequencia de uma variável qualitativa ou categórica
-participants_study <- table(Tabela_1$study)
+# Frequencia de uma variável qualitativa ou categórica - Exemplo
+participants_study <- table(data$study)
 participants_study
-sexo <- table(Tabela_1$sexo)
+sexo <- table(data$sexo)
 sexo
 
-# Média de uma variável quantitativa
-mean(Tabela_1$idade)
+# Média de uma variável quantitativa - Exemplo
+mean(data$idade)
 
-# Média removendo dados faltantes ou missing data (NA)
-mean(Tabela_1$perct_vida_salvador)
-mean(Tabela_1$perct_vida_salvador, na.rm = TRUE)
-mean(Tabela_1$smovosq_prev, na.rm = TRUE)
-summary(Tabela_1$smovosq_prev, na.rm = TRUE)
+# Média removendo dados faltantes ou missing data (NA) - Exemplo
+mean(data$perct_vida_salvador)
+mean(data$perct_vida_salvador, na.rm = TRUE)
+mean(data$smovosq, na.rm = TRUE)
+summary(data$smovosq, na.rm = TRUE)
+mean(data$smovosq_prev, na.rm = TRUE)
+summary(data$smovosq_prev, na.rm = TRUE)
 
-# Desvio padrão da média
-sd(Tabela_1$idade, na.rm = TRUE)
-sd(Tabela_1$perct_vida_salvador, na.rm = TRUE)
-sd(Tabela_1$smovosq_prev, na.rm = TRUE)
-sd(Tabela_2$smovosq, na.rm = TRUE)
+# Desvio padrão da média - Exemplo
+sd(data$idade, na.rm = TRUE)
+sd(data$perct_vida_salvador, na.rm = TRUE)
+sd(data$smovosq, na.rm = TRUE)
+sd(data$smovosq_prev, na.rm = TRUE)
 
-# Média geométrica
-exp(mean(log(Tabela_1$smovosq_prev), na.rm = TRUE))
 
-# Análise estratificada utilizando a função subset
-# Dados SB2024
-sm_idade_menor30<-subset(Tabela_1, idade<30)
+# Média geométrica - Exemplo
+exp(mean(log(data$smovosq), na.rm = TRUE))
+exp(mean(log(data$smovosq_prev), na.rm = TRUE))
+
+# Análise estratificada utilizando a função subset - Exemplo
+sm_idade_menor30<-subset(data, idade<30)
 sm_idade<-table(sm_idade_menor30$sm)
 sm_idade
 mean(sm_idade_menor30$smovosq, na.rm = TRUE)
 sd(sm_idade_menor30$smovosq, na.rm = TRUE)
 exp(mean(log(sm_idade_menor30$smovosq), na.rm = TRUE))
-sm_idade_maior30<-subset(Tabela_1, idade>=30)
+sm_idade_maior30<-subset(data, idade>=30)
 mean(sm_idade_maior30$smovosq, na.rm = TRUE)
 sd(sm_idade_maior30$smovosq, na.rm = TRUE)
 exp(mean(log(sm_idade_maior30$smovosq), na.rm = TRUE))
 
-# Análise estratificada
+###
+### Análise Urban Schisto - Tabela 2
+###
+# Carregando os dados de um arquivo .csv
+data <- read.csv("C:/Users/luciano.kalabric/OneDrive - FIOCRUZ/Projetos/2014- Mitermayer Esquistossomose urbana/Produção/Publicações/2023 Artigo Urban Schisto/Tabela 2 - All.csv")
 
 # Teste de qui-quadrado
-stu_data = table(Tabela_1$sexo,Tabela_1$sm_prev)
+# stu_data = table(data$sexo,data$smansoni) # Só em SB2015
+stu_data = table(data$sexo,data$sm)         # Todos os estudos
+print(stu_data)
+chisq.test(stu_data)
+stu_data = table(data$sexo,data$sm_prev)    # Só estudos com piloto
 print(stu_data)
 chisq.test(stu_data)
 
@@ -69,14 +87,14 @@ chisq.test(stu_data)
 
 # 1) Comparing one-sample mean to a standard known mean
 # 1.1) One-sample T-test (parametric)
-t.test(Tabela_1$idade, mu = 0, alternative = "two.sided")
-ggboxplot(Tabela_1$idade, na.rm = TRUE,
+t.test(data$idade, mu = 0, alternative = "two.sided")
+ggboxplot(data$idade, na.rm = TRUE,
           ylab = "Age (years)", xlab = FALSE,
           ggtheme = theme_minimal())
-shapiro.test(Tabela_1$idade) # Shapiro-Wilk normality test 
+shapiro.test(data$idade) # Shapiro-Wilk normality test 
 # 1.2) One-sample Wilcoxon test (non-parametric)
-wilcox.test(Tabela_1$idade, mu = 0, alternative = "two.sided")
-group_by(Tabela_1, sm_prev) %>%
+wilcox.test(data$idade, mu = 0, alternative = "two.sided")
+group_by(data, sm_prev) %>%
   summarise(
     count = n(),
     mean = mean(idade, na.rm = TRUE),
@@ -85,22 +103,22 @@ group_by(Tabela_1, sm_prev) %>%
 
 # 2) Comparing the means of two independent groups
 # 2.1) Unpaired two samples t-test (parametric)
-ggboxplot(Tabela_1, x = "sm_prev", y = "idade", 
+ggboxplot(data, x = "sm_prev", y = "idade", 
           color = "sm_prev", palette = c("#00AFBB", "#E7B800"),
           ylab = "Idade", xlab = "Groups")
 # Shapiro-Wilk normality test for Men's weights
-with(Tabela_1, shapiro.test(idade[sm_prev == 1])) # p = 0.1
+with(data, shapiro.test(idade[sm_prev == 1])) # p = 0.1
 # Shapiro-Wilk normality test for Women's weights
-with(Tabela_1, shapiro.test(idade[sm_prev == 2])) # p = 0.6
-var.test(idade ~ sm_prev, data = Tabela_1)
-t.test(idade ~ sm_prev, data = Tabela_1, var.equal = TRUE)
+with(data, shapiro.test(idade[sm_prev == 2])) # p = 0.6
+var.test(idade ~ sm_prev, data = data)
+t.test(idade ~ sm_prev, data = data, var.equal = TRUE)
 
 # 3) Comparing the means of paired samples
 # 4) Comparing the means of more than two groups
 # 5) MANOVA test: Multivariate analysis of variance
 # 6) Kruskal-Wallis test
-levels(Tabela_1$sm_prev)
-group_by(Tabela_1, sm_prev) %>%
+levels(data$sm_prev)
+group_by(data, sm_prev) %>%
   summarise(
     count = n(),
     mean = mean(idade, na.rm = TRUE),
@@ -108,56 +126,114 @@ group_by(Tabela_1, sm_prev) %>%
     median = median(idade, na.rm = TRUE),
     IQR = IQR(idade, na.rm = TRUE)
   )
-ggboxplot(Tabela_1, x = "sm_prev", y = "idade", 
+ggboxplot(data, x = "sm_prev", y = "idade", 
           color = "sm_prev", palette = c("#00AFBB", "#E7B800"),
           ylab = "Idade", xlab = "Groups")
-ggline(Tabela_1, x = "sm_prev", y = "idade", 
+ggline(data, x = "sm_prev", y = "idade", 
        add = c("mean_se", "jitter"), 
        ylab = "Idade", xlab = "SM")
-kruskal.test(idade ~ sm_prev, data = Tabela_1)
+kruskal.test(idade ~ sm_prev, data = data)
+
+# Análise estratificada a função group_by
 
 ###
-### Análise Urban Schisto
+### Análise Urban Schisto - Tabela 2
 ###
+# Comparação de médias
+levels(data$sm_prev)
+group_by(data, sm_prev) %>%
+  summarise(
+    count = n(),
+    mean = mean(perct_vida_salvador, na.rm = TRUE),
+    sd = sd(perct_vida_salvador, na.rm = TRUE),
+    median = median(perct_vida_salvador, na.rm = TRUE),
+    IQR = IQR(perct_vida_salvador, na.rm = TRUE)
+  )
+ggboxplot(data, x = "sm_prev", y = "perct_vida_salvador", 
+          color = "sm_prev", palette = c("#00AFBB", "#E7B800"),
+          ylab = "%Life in Salvador", xlab = "Groups")
+ggline(data, x = "sm_prev", y = "perct_vida_salvador", 
+       add = c("mean_se", "jitter"), 
+       ylab = "%Life in Salvador", xlab = "SM")
+kruskal.test(perct_vida_salvador ~ sm_prev, data = data)
 
-# Tabela 2
+###
+### Análise Urban Schisto - Tabela 3
+###
+# Carregando os dados de um arquivo .csv
 # Dados SA2019
-Tabela_2 <- read.csv("C:/Users/luciano.kalabric/OneDrive - FIOCRUZ/Projetos/2014- Mitermayer Esquistossomose urbana/Dados da pesquisa/2019 SA Relatório final coorte Tulane-IRB & CEP-IGM.csv")
+data3 <- read.csv("C:/Users/luciano.kalabric/OneDrive - FIOCRUZ/Projetos/2014- Mitermayer Esquistossomose urbana/Produção/Publicações/2023 Artigo Urban Schisto/Tabela 3 - SA2019.csv")
 id_corte<-4000
 # Dados SA2023
-Tabela_2 <- read.csv("C:/Users/luciano.kalabric/OneDrive - FIOCRUZ/Projetos/2014- Mitermayer Esquistossomose urbana/Dados da pesquisa/2023 SA Relatório final coorte Tulane-IRB & CEP-IGM.csv")
+data3 <- read.csv("C:/Users/luciano.kalabric/OneDrive - FIOCRUZ/Projetos/2014- Mitermayer Esquistossomose urbana/Produção/Publicações/2023 Artigo Urban Schisto/Tabela 3 - SA2023.csv")
 id_corte<-5000
 # Dados PI2021
-Tabela_2 <- read.csv("C:/Users/luciano.kalabric/OneDrive - FIOCRUZ/Projetos/2014- Mitermayer Esquistossomose urbana/Dados da pesquisa/2021 PI Relatório final coorte Tulane-IRB & CEP-IGM.csv")
+data3 <- read.csv("C:/Users/luciano.kalabric/OneDrive - FIOCRUZ/Projetos/2014- Mitermayer Esquistossomose urbana/Produção/Publicações/2023 Artigo Urban Schisto/Tabela 3 - PI2021.csv")
 id_corte<-3000
 # Dados PI2023
-Tabela_2 <- read.csv("C:/Users/luciano.kalabric/OneDrive - FIOCRUZ/Projetos/2014- Mitermayer Esquistossomose urbana/Dados da pesquisa/2023 PI Relatório final coorte Tulane-IRB & CEP-IGM.csv")
+data3 <- read.csv("C:/Users/luciano.kalabric/OneDrive - FIOCRUZ/Projetos/2014- Mitermayer Esquistossomose urbana/Produção/Publicações/2023 Artigo Urban Schisto/Tabela 3 - PI2023.csv")
 id_corte<-4000
 # Dados SB2024
-Tabela_2 <- read.csv("C:/Users/luciano.kalabric/OneDrive - FIOCRUZ/Projetos/2014- Mitermayer Esquistossomose urbana/Dados da pesquisa/2024 SB Relatório final coorte Tulane-IRB & CEP-IGM.csv")
+data3 <- read.csv("C:/Users/luciano.kalabric/OneDrive - FIOCRUZ/Projetos/2014- Mitermayer Esquistossomose urbana/Produção/Publicações/2023 Artigo Urban Schisto/Tabela 3 - SB2024.csv")
 id_corte<-3000
-sm_prev <- table(Tabela_2$sm)
+
+# Prevalência Global
+sm_prev <- table(data3$sm)
 sm_prev
-mean(Tabela_2$smovosq, na.rm = TRUE)
-sd(Tabela_2$smovosq, na.rm = TRUE)
-exp(mean(log(Tabela_2$smovosq), na.rm = TRUE))
-# Coorte
-coorte <- subset(Tabela_2, id<id_corte)
+mean(data3$smovosq, na.rm = TRUE)
+sd(data3$smovosq, na.rm = TRUE)
+exp(mean(log(data3$smovosq), na.rm = TRUE))
+# Prevalência da Coorte (Incidência)
+coorte <- subset(data3, id<id_corte)
 sm_coorte <- table(coorte$sm)
 sm_coorte
 mean(coorte$smovosq, na.rm = TRUE)
 sd(coorte$smovosq, na.rm = TRUE)
 exp(mean(log(coorte$smovosq), na.rm = TRUE))
-# Novos
-novos <- subset(Tabela_2, id>=id_corte)
+# Prevalência dos Novos
+novos <- subset(data3, id>=id_corte)
 sm_novos <- table(novos$sm)
 sm_novos
 mean(novos$smovosq, na.rm = TRUE)
 sd(novos$smovosq, na.rm = TRUE)
 exp(mean(log(novos$smovosq), na.rm = TRUE))
 
+
+# Dados SB2015
+data3 <- read.csv("C:/Users/luciano.kalabric/OneDrive - FIOCRUZ/Projetos/2014- Mitermayer Esquistossomose urbana/Produção/Publicações/2023 Artigo Urban Schisto/Tabela 3 - SB2015.csv")
+
+# Prevalência Global
+sm_prev <- table(data3$smansoni)
+sm_prev
+mean(data3$smovosq, na.rm = TRUE)
+sd(data3$smovosq, na.rm = TRUE)
+exp(mean(log(data3$smovosq), na.rm = TRUE))
+# Prevalência da Coorte (Incidência)
+coorte <- subset(data3, part2011==1)
+sm_coorte <- table(coorte$smansoni)
+sm_coorte
+mean(coorte$smovosqmedia, na.rm = TRUE)
+sd(coorte$smovosqmedia, na.rm = TRUE)
+exp(mean(log(coorte$smovosqmedia), na.rm = TRUE))
+# Prevalência dos Novos
+novos <- subset(data3, part2011==2)
+sm_novos <- table(novos$smansoni)
+sm_novos
+mean(novos$smovosqmedia, na.rm = TRUE)
+sd(novos$smovosqmedia, na.rm = TRUE)
+exp(mean(log(novos$smovosqmedia), na.rm = TRUE))
+
+
+
+
+
+
+
+
+
 # Figura 1
-sm_by_studygroup <- subset(Tabela_1, study=="DC2017", select=c(idade, sexo, sm, sm_prev))
+data2 <- read.csv("C:/Users/luciano.kalabric/OneDrive - FIOCRUZ/Projetos/2014- Mitermayer Esquistossomose urbana/Produção/Publicações/2023 Artigo Urban Schisto/Tabela 2 - All.csv")
+sm_by_studygroup <- subset(data2, study=="DC2017", select=c(idade, sexo, sm, sm_prev))
 sm_by_studygroup$faixaetaria <- NA
 
 # Recodifica idade por grupo etário
@@ -173,21 +249,3 @@ participants_sm <- table(sm_by_studygroup$faixaetaria, sm_by_studygroup$sm)
 participants_sm
 participants_sm_prev <- table(sm_by_studygroup$faixaetaria, sm_by_studygroup$sm_prev)
 participants_sm_prev
-
-# Tabela 3 - Kruskal-Wallis test
-levels(Tabela_1$sm_prev)
-group_by(Tabela_1, sm_prev) %>%
-  summarise(
-    count = n(),
-    mean = mean(perct_vida_salvador, na.rm = TRUE),
-    sd = sd(perct_vida_salvador, na.rm = TRUE),
-    median = median(perct_vida_salvador, na.rm = TRUE),
-    IQR = IQR(perct_vida_salvador, na.rm = TRUE)
-  )
-ggboxplot(Tabela_1, x = "sm_prev", y = "perct_vida_salvador", 
-          color = "sm_prev", palette = c("#00AFBB", "#E7B800"),
-          ylab = "%Life in Salvador", xlab = "Groups")
-ggline(Tabela_1, x = "sm_prev", y = "perct_vida_salvador", 
-       add = c("mean_se", "jitter"), 
-       ylab = "%Life in Salvador", xlab = "SM")
-kruskal.test(perct_vida_salvador ~ sm_prev, data = Tabela_1)
