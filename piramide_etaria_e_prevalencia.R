178361f11808e21e56ddbe55fdb4434689eab51f
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2024-10-14 15:29:10 -0300

    Atualização

--- piramide_etaria_e_prevalencia.R
+++ piramide_etaria_e_prevalencia.R
@@ -4,8 +4,7 @@
 # Objetivo: Plotar um gráfico de pirâmide etária
 
 # Configurando o diretório de trabalho
-setwd("C:/Users/kalab/OneDrive/Documentos/GitHub/r_analysis")
-
+setwd("C:/Users/luciano.kalabric/OneDrive - FIOCRUZ/GitHub/r_analysis")
 
 # Instalando bibliotecas necessários
 list.of.packages <- c("ggplot2", "tidyverse", "dplyr")
@@ -37,97 +36,98 @@ dados <- dados %>%
 
 # 3. Plotar a pirâmide etária geral
 ggplot(dados, aes(x = faixa_etaria, fill = factor(sexo))) +
-  geom_bar(data = subset(dados, sexo == 1), aes(y = -..count..), stat = "count") +
-  geom_bar(data = subset(dados, sexo == 2), aes(y = ..count..), stat = "count") +
+  coord_flip() +
+  geom_bar(data = subset(dados, sexo == 1), aes(y = ..count..), stat = "count") +
+  geom_bar(data = subset(dados, sexo == 2), aes(y = -..count..), stat = "count") +
   #facet_wrap(~ study) +
   scale_fill_manual(values = c("1" = "blue", "2" = "red"),
                     labels = c("Male", "Female")) +
-  coord_flip() +
   labs(x = "Age group", y = "Frequency", fill = "Sex") +
   theme_minimal()
 
 # 3.1 Plotar a pirâmide etária para cada estudo
 ggplot(dados, aes(x = faixa_etaria, fill = factor(sexo))) +
-  geom_bar(data = subset(dados, sexo == 1), aes(y = -..count..), stat = "count") +
-  geom_bar(data = subset(dados, sexo == 2), aes(y = ..count..), stat = "count") +
-  facet_wrap(~ study) +
+  coord_flip() +
+  geom_bar(data = subset(dados, sexo == 1), aes(y = ..count..), stat = "count") +
+  geom_bar(data = subset(dados, sexo == 2), aes(y = -..count..), stat = "count") +
   scale_fill_manual(values = c("1" = "blue", "2" = "red"),
                     labels = c("Male", "Female")) +
-  coord_flip() +
   labs(x = "Age group", y = "Frequency", fill = "Sex") +
+  facet_wrap(~ study) +
   theme_minimal()
 
-# 5. Calcula a prevalência por faixa etária
+# 4. Calcula a prevalência por faixa etária
 prevalencia_por_idade <- dados %>% 
   group_by(faixa_etaria) %>%
   summarise(
     total = n(),
     sm = sum(sm_prev == 1),
-    proporcao = (sm * 100 / total)
+    proporcao = round((sm * 100 / total), digits = 1)
   )
 
-# 5.1 Plotar a pirâmide etária para cada estudo
+# 4.1 Plotar a pirâmide etária para cada estudo
 ggplot(prevalencia_por_idade, aes(x = faixa_etaria, y = proporcao)) +
-  geom_bar(stat = "identity", position = "dodge") +
   coord_flip() +
+  geom_bar(stat = "identity", position = "dodge") +
+  geom_text(aes(y = proporcao, label = proporcao), vjust = 0.5, hjust = 1.3) +
   labs(x = "Age group", y = "SM Prevalence (%)") +
   theme_minimal()
 
-# 6. Calcula a prevalência por sexo e faixa etária
+# 5. Calcula a prevalência por sexo e faixa etária
 prevalencia_por_sexo_idade <- dados %>% 
   group_by(sexo, faixa_etaria) %>%
   summarise(
     total = n(),
     sm = sum(sm_prev == 1),
-    proporcao = (sm * 100 / total)
+    proporcao = round((sm * 100 / total), digits = 1)
   )
 
-# 6.1 Plotar a prevalência por sexo e faixa etária
+# 5.1 Plotar a prevalência por sexo e faixa etária
 ggplot(prevalencia_por_sexo_idade, aes(x = faixa_etaria, y = proporcao, fill = factor(sexo))) +
+  coord_flip() +
   geom_bar(data = subset(prevalencia_por_sexo_idade, sexo == 1), aes(y = proporcao), stat = "identity", position = "dodge") +
+  geom_text(data = subset(prevalencia_por_sexo_idade, sexo == 1), aes(y = proporcao, label = proporcao), vjust = 0.5, hjust = -0.1) +
   geom_bar(data = subset(prevalencia_por_sexo_idade, sexo == 2), aes(y = -proporcao), stat = "identity", position = "dodge") +
-  #facet_wrap(~ study) +
+  geom_text(data = subset(prevalencia_por_sexo_idade, sexo == 2), aes(y = -proporcao, label = proporcao), vjust = 0.5, hjust = 1.2) +
   scale_fill_manual(values = c("1" = "blue", "2" = "red"),
                     labels = c("Male", "Female")) +
-  coord_flip() +
-  labs(x = "Age group", y = "SM prevalence (%)", fill = "Sex") +
+    labs(x = "Age group", y = "SM prevalence (%)", fill = "Sex") +
+  #facet_wrap(~ study) +
   theme_minimal()
 
-# 7. Calcula a prevalência por estudo
+# 6. Calcula a prevalência por estudo
 prevalencia_por_estudo <- dados %>% 
   group_by(study, faixa_etaria) %>%
   summarise(
     total = n(),
     sm = sum(sm_prev == 1),
-    proporcao = (sm * 100 / total)
+    proporcao = round((sm * 100 / total), digits = 1)
   )
 
-view(prevalencia_por_estudo)
-
-# 7.1 Plotar a prevalência por estudo
+# 6.1 Plotar a prevalência por estudo
 ggplot(prevalencia_por_estudo, aes(x = faixa_etaria, y = proporcao)) +
-  geom_bar(stat = "identity", position = "dodge") +
-  facet_wrap(~ study) +
   coord_flip() +
+    geom_bar(stat = "identity", position = "dodge") +
   labs(x = "Age group", y = "SM prevalence (%)") +
+  facet_wrap(~ study) +
   theme_minimal()
 
-# 8. Calcula a prevalência por sexo, faixa etária e por estudo
+# 7. Calcula a prevalência por sexo, faixa etária e por estudo
 prevalencia_por_agrupamento <- dados %>% 
   group_by(study, sexo, faixa_etaria) %>%
   summarise(
     total = n(),
     sm = sum(sm_prev == 1),
-    proporcao = (sm * 100 / total)
+    proporcao = round((sm * 100 / total), digits = 1)
   )
 
-# 8.1 Plotar a prevalência por sexo, faixa etária e por estudo
+# 7.1 Plotar a prevalência por sexo, faixa etária e por estudo
 ggplot(prevalencia_por_agrupamento, aes(x = faixa_etaria, y = proporcao, fill = factor(sexo))) +
-  geom_bar(data = subset(prevalencia_por_agrupamento, sexo == 1), aes(y = proporcao), stat = "identity", position = "dodge") +
+  coord_flip() +
+    geom_bar(data = subset(prevalencia_por_agrupamento, sexo == 1), aes(y = proporcao), stat = "identity", position = "dodge") +
   geom_bar(data = subset(prevalencia_por_agrupamento, sexo == 2), aes(y = -proporcao), stat = "identity", position = "dodge") +
-  facet_wrap(~ study) +
   scale_fill_manual(values = c("1" = "blue", "2" = "red"),
                     labels = c("Male", "Female")) +
-  coord_flip() +
   labs(x = "Age group", y = "SM prevalence (%)", fill = "Sex") +
+  facet_wrap(~ study) +
   theme_minimal()
