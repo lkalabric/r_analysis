e42af89f680ff3abc2b611dea3826d0307fd4217
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2024-12-25 19:05:06 -0300

    Fim por hoje

--- redcap_analysis.R
+++ redcap_analysis.R
@@ -4,27 +4,45 @@
 # Link: https://www.dataquest.io/blog/r-api-tutorial/
 
 # Instala as biblioteca básicas para conexão API no R
-install.packages(c("httr", "jsonlite"))inst
+list.of.packages <- c("httr", "jsonlite")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
 
 # Testa as bibliotecas
 library(httr)
 library(jsonlite)
 
-# API Playground REDCap
-#!/usr/bin/env Rscript
-token <- "39169DDCCC39AED2489284B828E0A65A"
-url <- "https://bdp.bahia.fiocruz.br/api/"
-formData <- list("token"=token,
+
+setwd("C:/Users/kalab/OneDrive - FIOCRUZ/GitHub/r_analysis/redcap_analysis")
+source("config.R")   # Lê api_token e api_url do projeto
+
+## API Playground REDCap
+
+# Export Project Information
+formData <- list("token"=api_token,
     content='project',
     format='json',
     returnFormat='json'
 )
-response <- httr::POST(url, body = formData, encode = "form")
-result <- httr::content(response)
-print(result)
+response <- httr::POST(api_url, body = formData, encode = "form")
+project <- httr::content(response)
+print(project)
 
+# Export Records
+formData <- list("token"=api_token,
+                 content='record',
+                 format='csv',
+                 type='flat'
+)
+response <- httr::POST(api_url, body = formData, encode = "form")
+result_df <- httr::content(response)
+
+# Estatísticas descritivas dos nossos dados
+table(result_df$sexo)
+summary(result_df$idade)
 
 # Export records to R
+# A biblioteca RCurl está dando erro no R, mas funcionou no Google Colab!!!
 install.packages("RCurl")
 
 setwd("C:/Users/kalab/OneDrive - FIOCRUZ/GitHub/r_analysis")
