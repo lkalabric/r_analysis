06f5822cbc73822657df0c1ba07a88748bbd3344
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2025-01-13 17:37:27 -0300

    Estudo sobre a curva ROC

--- curvaROC.R
+++ curvaROC.R
@@ -1,1 +1,20 @@
+# Estatística descritiva para análise de dados
+# Trabalhando pROC-package {pROC}
+# Autor: Luciano K. Silva
 
+# Installing and using
+list.of.packages <- c("pROC", "dplyr")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
+library(pROC)
+
+library(dplyr)
+# Experimental: pipelines
+View(aSAH)
+aSAH %>% 
+  filter(gender == "Female") %>%
+  roc(outcome, s100b)
+
+gender <- table(aSAH$gender)
+gender
